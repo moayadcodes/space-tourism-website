c005cb2f9d0d07f0c68654882f7de9cb7d1de95b
moayadcodes <moayad.farah@outlook.com>
2023-02-15 13:19:46 -0500

    Added ESLint.

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  'parser': 'vue-eslint-parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'parser': '@typescript-eslint/parser',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'vue',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'comma-dangle': [
+      'error',
+      'always-multiline',
+    ],
+    'indent': [
+      'error',
+      2,
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'semi': [
+      'error',
+      'always',
+    ],
+  },
+};
