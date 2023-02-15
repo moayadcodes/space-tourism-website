2a8ec8a0dbba01cf650904d985e2397762745060
moayadcodes <moayad.farah@outlook.com>
2023-02-15 13:05:58 -0500

    Added TypeScript and enabled type checking, and disabled auto imports.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  imports: {
+    autoImport: false,
+  },
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+});
