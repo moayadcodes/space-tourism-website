6e0d2fc97d3b1de8b5241fc0ed9a49aae3d89544
moayadcodes <moayad.farah@outlook.com>
2023-02-16 09:25:50 -0500

    Added Tailwind CSS.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: [
+    '~/assets/css/main.css',
+  ],
   imports: {
     autoImport: false,
   },
   modules: [
     '@nuxt/content',
   ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   typescript: {
     strict: true,
     typeCheck: true,
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{js,ts,vue}',
+    './pages/**/*.{js,ts,vue}',
+    './app.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
